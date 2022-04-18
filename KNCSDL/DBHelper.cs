c199c675300c7e34d37142600cd44eebd5289c1d
Vien Nguyen <viennguyen.280202@gmail.com>
2022-04-18 20:06:20 +0700

    first

---
+++ KNCSDL/DBHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KNCSDL
+{
+
+    public class DBHelper
+    {
+        private SqlConnection cnn;
+        public DBHelper(string s)
+        {
+            cnn = new SqlConnection(s);
+        }
+
+        public void ExecuteDB(string query)
+        {
+            SqlCommand cmd = new SqlCommand(query,cnn);
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+        public DataTable GetRecordSV(string query)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[]
+            {
+                new DataColumn{ColumnName = "MSSV", DataType = typeof(string)},
+                new DataColumn{ColumnName = "NameSV", DataType = typeof(string)},
+                new DataColumn{ColumnName = "DTB", DataType = typeof(float)},
+                new DataColumn{ColumnName = "ID_Lop", DataType = typeof(string)},
+            });
+            SqlCommand cmd = new SqlCommand(query, cnn);
+            SqlDataReader r;
+            cnn.Open();
+            r = cmd.ExecuteReader();
+            while (r.Read())
+            {
+                dt.Rows.Add(
+                    r["MSSV"].ToString(),
+                    r["NameSV"].ToString(),
+                    r["DTB"].ToString(),
+                    r["ID_Lop"].ToString()
+                    );
+            }
+            cnn.Close();
+            return dt;
+        }
+    }
+}
---
+++ KNCSDL/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KNCSDL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string MSSV, NameSV, ID_Lop;
+        float DTB;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = @"Data Source=DESKTOP-530ACSJ;Initial Catalog=QLSV;Integrated Security=True";
+            DBHelper db = new DBHelper(s);
+            string query = "Select * from SV";
+            dataGridView1.DataSource = db.GetRecordSV(query);
+
+
+            //SqlConnection cnn = new SqlConnection(s);
+            ////string query = "INSERT INTO SV (MSSV, NameSv, DTB,ID_Lop) VALUES(7, 'Pham Van Tien Truong7', 7.6, 1); ";
+            ////string query = "Select * from SV Where NameSV ='"+textBox1.Text+"'";
+            //SqlParameter p = new SqlParameter
+            //{
+            //    ParameterName = "@Name",
+            //    Value = textBox1.Text
+            //};
+            //string query = "Select * from SV Where NameSV = @Name";
+            //SqlCommand cmd = new SqlCommand(query, cnn);
+            //cmd.Parameters.Add(p);
+            //cnn.Open();
+            ////int n = (int)cmd.ExecuteScalar();
+            ////cmd.ExecuteNonQuery();
+            //DataTable dt = new DataTable();
+            //dt.Columns.AddRange(new DataColumn[]
+            //{
+            //    new DataColumn{ColumnName = "MSSV", DataType = typeof(string)},
+            //    new DataColumn{ColumnName = "NameSV", DataType = typeof(string)},
+            //    new DataColumn{ColumnName = "DTB", DataType = typeof(float)},
+            //    new DataColumn{ColumnName = "ID_Lop", DataType = typeof(string)},
+            //});
+            //SqlDataReader r;
+            //r = cmd.ExecuteReader();
+            //while (r.Read())
+            //{
+            //    dt.Rows.Add(
+            //        r["MSSV"].ToString(),
+            //        r["NameSV"].ToString(),
+            //        r["DTB"].ToString(),
+            //        r["ID_Lop"].ToString()
+            //        );
+            //}
+            //cnn.Close();
+            //dataGridView1.DataSource = dt;
+        }
+    }
+}
